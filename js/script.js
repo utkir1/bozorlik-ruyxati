b9c4411996aa1cdb3688971453ca8872de52a20a
utkir1 <otkirsoatboyev1@gmail.com>
2022-04-25 16:15:38 +0500

    added checkbox

--- js/script.js
+++ js/script.js
@@ -1,28 +1,37 @@
-
-let elInput = document.querySelector (".product-input");
-let elForm = document.querySelector (".form");
-let elClearBtn = document.querySelector (".clear-btn");
-let elList = document.querySelector (".products-list");
+let elInput = document.querySelector(".product-input");
+let elForm = document.querySelector(".form");
+let elClearBtn = document.querySelector(".clear-btn");
+let elList = document.querySelector(".products-list");
+let elCheck = document.querySelector(".product-check");
 let productList = [];
 
-elForm.addEventListener("submit", function(evt){
+elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
+  let checkActive = elCheck.checked;
   let itemlist = elInput.value.trim();
-  if( itemlist.length > 10 || itemlist == "" || !isNaN(itemlist) || itemlist.length < 2){
-    elInput.value = "";
-    elInput.style="border-color: red";
+  if (itemlist.length > 10 || itemlist == "" || !isNaN(itemlist) || itemlist.length < 2) {
+    elInput.setAttribute("style", "border-color: red");
     return;
   }
-  elInput.style="border-color: black";
+  elInput.setAttribute("style", "border-color: black");
+  if (checkActive) {
+    productList.unshift(itemlist);
+  }
+  else{
   productList.push(itemlist);
+  }
   elList.innerHTML = null;
   for (let product of productList) {
     let newLi = document.createElement("li");
     newLi.textContent = product;
+    newLi.setAttribute("class", " p-2 w-100 rounded-pill  text-capitalize fw-bold")
     elList.appendChild(newLi);
   }
   elInput.value = null;
-})
-elClearBtn.addEventListener("click", function(evt){
+
+} )
+
+
+elClearBtn.addEventListener("click", function (evt) {
   elList.innerHTML = null
 })
